a7cbd1dac5b6a65b73403c432ea92fb3f3469547
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-07-25 10:25:23 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,8 @@
+def create_youtube_video(title,descriotion):
+	uvid={"title";title,"description";descriotion,"likes";likes,"dislikes";dislikes}
+	comments={"username";comment}
+	return uvid
+def like (uvid);
+
+	
+	
